6d67592ea8a2057d2e76cb8afe228da619e6b659
Andrew Kelley <superjoe30@gmail.com>
2015-08-26 02:37:49 -0700

    add generated docs

---
+++ www/doc/examples.js
@@ -0,0 +1,6 @@
+var examples =
+[
+    [ "sio_list_devices.c", "sio_list_devices_8c-example.html", null ],
+    [ "sio_microphone.c", "sio_microphone_8c-example.html", null ],
+    [ "sio_sine.c", "sio_sine_8c-example.html", null ]
+];
