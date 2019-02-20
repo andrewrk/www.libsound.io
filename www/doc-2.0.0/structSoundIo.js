39b46a98334bf17dadf823bffae7cc0ea0db9175
Andrew Kelley <andrew@ziglang.org>
2019-02-20 08:34:26 -0500

    hastily add the new release

---
+++ www/doc-2.0.0/structSoundIo.js
@@ -0,0 +1,12 @@
+var structSoundIo =
+[
+    [ "app_name", "structSoundIo.html#a4d7782959728999bd6dcc8c7633a0d8a", null ],
+    [ "current_backend", "structSoundIo.html#ac5f9243c5c296f7b073c4fe1831d4d8d", null ],
+    [ "emit_rtprio_warning", "structSoundIo.html#aabc76c59d27e482f3ad1ebe89e458a16", null ],
+    [ "jack_error_callback", "structSoundIo.html#affbbb30d612dbcc2ef106b405ad57b6a", null ],
+    [ "jack_info_callback", "structSoundIo.html#a5b5aff1330d5fba518b0257a4082ffcb", null ],
+    [ "on_backend_disconnect", "structSoundIo.html#a9e9289c23d5f9ec665ad098127b4a614", null ],
+    [ "on_devices_change", "structSoundIo.html#a3ea585f83c5d0c6514a846754828b80d", null ],
+    [ "on_events_signal", "structSoundIo.html#aa553107391f8421e387d87c2fef30a19", null ],
+    [ "userdata", "structSoundIo.html#abf3f0a91152aec0089ef78295e7e5c92", null ]
+];
---
+++ www/doc-2.0.0/structSoundIoOutStream.js
@@ -0,0 +1,18 @@
+var structSoundIoOutStream =
+[
+    [ "bytes_per_frame", "structSoundIoOutStream.html#a799cd8220282eee3d21b6a0511e9533a", null ],
+    [ "bytes_per_sample", "structSoundIoOutStream.html#a873db96a2513e4875887c8ff66ff97b2", null ],
+    [ "device", "structSoundIoOutStream.html#a2da7fdc9563365897c1dd454e500f7e9", null ],
+    [ "error_callback", "structSoundIoOutStream.html#ac3853db7f65cd3ab4cfddcc86295bc57", null ],
+    [ "format", "structSoundIoOutStream.html#a164722836745394086aefb53f0a0c5c8", null ],
+    [ "layout", "structSoundIoOutStream.html#a37cb9fa2bb2693794142f3a52486c839", null ],
+    [ "layout_error", "structSoundIoOutStream.html#a1e8831ac68aab9bd3a411a758b096bff", null ],
+    [ "name", "structSoundIoOutStream.html#a3e3f869a0af4ef1914a864c93ce8d3c2", null ],
+    [ "non_terminal_hint", "structSoundIoOutStream.html#a6da85b1b8d2ef47b73774fa09fb12565", null ],
+    [ "sample_rate", "structSoundIoOutStream.html#a7c2d76e17c5031a589879522b7a823d1", null ],
+    [ "software_latency", "structSoundIoOutStream.html#a20aac1422d3cc64b679616bb8447f06d", null ],
+    [ "underflow_callback", "structSoundIoOutStream.html#a610870a5304701ec08d410243c2eb328", null ],
+    [ "userdata", "structSoundIoOutStream.html#a14549e062b980bf32254718403c55ca2", null ],
+    [ "volume", "structSoundIoOutStream.html#a45acf8fa70a000aada2665bf66b8d8f1", null ],
+    [ "write_callback", "structSoundIoOutStream.html#a84e7d3ddeda29573d547414bd0a1f010", null ]
+];
